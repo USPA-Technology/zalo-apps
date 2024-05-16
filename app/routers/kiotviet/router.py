bfa7bd45273adc1650e5fa4fd09d4416b8a28160
baolamdev <baolam.main@gmail.com>
2024-05-16 17:20:12 +0700

    update data model

--- app/routers/kiotviet/router.py
+++ app/routers/kiotviet/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, Request
 from pydantic import ValidationError
-from .schema import ModelKiotViet
+from .schema import ModelKiotViet, Datum
 from ...dependencies import is_valid_signature
 from ...core.config import SECRET_KEY_WEBHOOK
 
@@ -9,16 +9,17 @@ signature = SECRET_KEY_WEBHOOK
 router  = APIRouter()
 
 
+ModelKiotViet.Notifications
+
 @router.post("kiotviet/webhook/{secret}")
 async def receive_webhook(data: ModelKiotViet, secret: str):
     if not is_valid_signature(secret, signature):
         raise HTTPException(status_code=400, detail="Invalid signature")
     try:
         model_data = data.model_dump()
-        return {"message": "Webhook received", "data": model_data}
+        return {"message": "Webhook received", "data_customer_name": Datum.CustomerName}
     except ValidationError as e:
         raise HTTPException(status_code=400, detail=str(e))
     except Exception as e:
         raise HTTPException(status_code=500, detail="Internal Server Error")
-    
     
