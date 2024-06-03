5883b37fc3acbdb33d659562378ee22ad28b27bb
baolamdev <baolam.main@gmail.com>
2024-06-03 18:21:57 +0700

    update code

--- app/routers/zalo/router.py
+++ app/routers/zalo/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from fastapi import HTTPException
-from core.config import ACCESS_TOKEN_ZALO
+from core.config import ACCESS_TOKEN_ZALO, ACCESS_TOKEN_ZALO_OA_EVERON
 import httpx
 import json
 import logging
@@ -21,7 +21,7 @@ logger = ProcessedItemLogger()
 @router.get('/zalo/users')
 async def get_user_list_id(
     offset: int = 0,
-    count: int = 50,
+    count: int = 10,
     tag_name: str = None,
     last_interation_period: str = None,
     is_follower: bool = True,
@@ -29,7 +29,7 @@ async def get_user_list_id(
 ):
 
     api_url = "https://openapi.zalo.me/v3.0/oa/user/getlist"
-    access_token = ACCESS_TOKEN_ZALO
+    access_token = ACCESS_TOKEN_ZALO_OA_EVERON
     headers = {
         "access_token": access_token
     }
@@ -44,7 +44,7 @@ async def get_user_list_id(
                 "count": count,
                 # "tag_name": tag_name,
                 # "last_interaction_period": last_interation_period,
-                # "is_follower": is_follower,
+                "is_follower": is_follower,
             })
         }
         try:
@@ -52,21 +52,22 @@ async def get_user_list_id(
                 response = await client.get(url=api_url, headers=headers, params=params)
                 response.raise_for_status()
                 response_result = response.json()
-                model_users = ModelListUser(**response_result)
-                list_user_id = model_users.data.users
+                return response_result
+                # model_users = ModelListUser(**response_result)
+                # list_user_id = model_users.data.users
             
-                if not list_user_id:
-                    logging.info("No more users to process import data of from Zalo OA")
-                    break
-                for user in list_user_id:
-                    # if offset > last_processed_item:
-                        user_id_str = str(user.user_id)
-                        await get_user_info(user_id_str)
-                        total_user_id.append(user_id_str)
-                        # logger.log_processed_item()
-                        logging.info(f'Processed offset: {offset}')
+                # if not list_user_id:
+                #     logging.info("No more users to process import data of from Zalo OA")
+                #     break
+                # for user in list_user_id:
+                #     # if offset > last_processed_item:
+                #         user_id_str = str(user.user_id)
+                #         await get_user_info(user_id_str)
+                #         total_user_id.append(user_id_str)
+                #         # logger.log_processed_item()
+                #         logging.info(f'Processed offset: {offset}')
                 
-                offset += count # Update offset by count to get the next set of result
+                # offset += count # Update offset by count to get the next set of result
             
         except httpx.RequestError as e:
             logging.error(f"Error connection with KiotViet: {e}")
@@ -101,7 +102,7 @@ async def get_users():
 @router.get('/getUserDetails/{user_id}')
 async def get_user_info(user_id: str):
     api_url = "https://openapi.zalo.me/v3.0/oa/user/detail"
-    access_token = ACCESS_TOKEN_ZALO
+    access_token = ACCESS_TOKEN_ZALO_OA_EVERON
     try:
         async with httpx.AsyncClient() as client:
             response = await client.get(api_url, 
