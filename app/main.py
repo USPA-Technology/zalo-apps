94b35a6fe0400cd4199435da6b001f358f45cb8b
baolamdev <baolam.main@gmail.com>
2024-06-04 17:16:31 +0700

    update code

--- app/main.py
+++ app/main.py
@@ -62,31 +62,3 @@ class ProcessedItemMiddleware(BaseHTTPMiddleware):
 logger = ProcessedItemLogger()
 app.add_middleware(ProcessedItemMiddleware, logger=logger)
 
-
-
-
-
-
-
-
-
-
-
-
-
-# @app.post("/tasks", status_code=201)
-# def run_task(payload = Body(...)):
-#     task_type = payload["type"]
-#     task = create_task.delay(int(task_type))
-#     return JSONResponse({"task_id": task.id})
-
-# @app.get("/tasks/{task_id}/")
-# def get_status(task_id):
-#     task_result = AsyncResult(task_id)
-#     result = {
-#         "task_id": task_id,
-#         "task_status": task_result.status,
-#         "task_result": task_result.result
-#     }
-#     return JSONResponse(result)
-
